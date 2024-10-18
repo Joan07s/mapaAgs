b105236733529056375a93140015322d9eddde3b
Joan07s <joansebastiancruzgonzalez1@gmail.com>
2024-10-18 13:54:39 -0600

    script.js

--- script.js
+++ script.js
@@ -11,15 +11,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker([21.88234, -102.28259]).addTo(map);
 marker.bindPopup("<b>¡Bienvenidos a Aguascalientes!</b><br>Capital del Estado.").openPopup();
 
-// Marcador en la Plaza de la Patria
+// Añadir más marcadores en lugares clave
 var plazaPatria = L.marker([21.88187, -102.29495]).addTo(map);
 plazaPatria.bindPopup("<b>Plaza de la Patria</b><br>Corazón de Aguascalientes.");
 
-// Marcador en el Museo Nacional de la Muerte
 var museoMuerte = L.marker([21.88417, -102.28878]).addTo(map);
 museoMuerte.bindPopup("<b>Museo Nacional de la Muerte</b><br>Un lugar único.");
 
-// Dibujar un círculo en el recinto de la Feria Nacional de San Marcos
+// Dibujar un círculo en la Feria Nacional de San Marcos
 var feriaSanMarcos = L.circle([21.87888, -102.29727], {
   color: 'green',
   fillColor: '#0f3',
@@ -28,7 +27,7 @@ var feriaSanMarcos = L.circle([21.87888, -102.29727], {
 }).addTo(map);
 feriaSanMarcos.bindPopup("<b>Feria Nacional de San Marcos</b><br>El evento más importante de Aguascalientes.");
 
-// Dibuja un polígono para delimitar el área del Centro Histórico
+// Dibuja un polígono para el Centro Histórico de Aguascalientes
 var centroHistorico = L.polygon([
   [21.8821, -102.2935],
   [21.8829, -102.2865],
